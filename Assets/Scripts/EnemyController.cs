94cfbc4fa57944274e50ad505cdcc64eb9cd0da3
wlvlrtm <kimsk200012@gmail.com>
2021-10-02 00:34:21 +0900

    적군 추적 기능 구현
    1) 이제 적들이 가로 본능을 강조하며 플레이어를 추적합니다.
    
    2) 이제 적들이 움직일 때 애니메이션이 적용됩니다.
    
    // !! //
    
    1) 적들이 제 방향으로 움직이지 않고 옆을 바라본 채로 플레이어를 추적합니다. 편집이 필요합니다.
    
    2) 아직 추격 애니메이션 적용 외에 다른 애니메이션들은 적용되어 있지 않습니다.

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -1,8 +1,92 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+
+public enum Enchant
+{
+    Elec,
+    Fire,
+    Ice,
+    Normal
+}
+
+public enum Stat
+{
+    Run,
+    Attack,
+    Idle,
+    Die
+}
+
 
 public class EnemyController : MonoBehaviour
 {
+    [SerializeField] private int life;
+    [SerializeField] private int attackPower;
+    //[SerializeField] private float speed;
+    [SerializeField] private int score;
 
+    // Temp //   
+    [SerializeField] private bool isRun;
+    [SerializeField] private bool isAttack;
+    [SerializeField] private bool isIdle;
+    [SerializeField] private bool doDie;
+
+
+    public Enchant enchant;
+    public GameObject player;
+
+    private Animator animator;
+    private NavMeshAgent nav;
+    private Rigidbody eRB;
+    private float smooth = 2.0f;
+
+    
+    private void Awake()
+    {
+        nav = GetComponent<NavMeshAgent>();
+        eRB = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
+
+        ChaseStart();
+    }
+
+
+    private void FixedUpdate()
+    {
+        FreezeVelocity();
+    }
+
+
+    private void ChaseStart()
+    {
+        isRun = true;
+        animator.SetBool("isRun", true);
+    }
+
+
+    private void Update()
+    {
+        if (isRun)  // Animation; Run
+        {
+            // Move To Player //
+            nav.SetDestination(player.transform.position);
+
+            Vector3 relativePos = (player.transform.position - gameObject.transform.position).normalized;
+            Quaternion rotation = Quaternion.LookRotation(relativePos, Vector3.up);
+            transform.rotation = rotation;
+        }
+        else
+        {
+            isIdle = true;
+        }
+    }
+
+
+    private void FreezeVelocity()
+    {
+        eRB.velocity = Vector3.zero;
+        eRB.angularVelocity = Vector3.zero;
+    }
 }
